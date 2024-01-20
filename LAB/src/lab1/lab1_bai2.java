82629f93bfbc242a272623f8c82bb44c2be82889
KhoaTrinh18 <126164079+KhoaTrinh18@users.noreply.github.com>
2024-01-20 21:32:51 +0700

    init

--- LAB/src/lab1/lab1_bai2.java
+++ LAB/src/lab1/lab1_bai2.java
@@ -6,9 +6,14 @@ public class lab1_bai2 {
 	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		System.out.println("Nhập chiều dài và chiều rộng của hình chữ nhật:");
 		System.out.print("Chiều dài: ");
 		float chieudai = scanner.nextFloat();
-		
+		System.out.print("Chiều rộng: ");
+		float chieurong = scanner.nextFloat();
+		float chuvi = (chieudai + chieurong) * 2;
+		float dientich = chieudai * chieurong;
+		System.out.printf("Diện tích: %.2f, Chu vi: %.2f, Cạnh nhỏ: %.2f", dientich, chuvi, Math.min(chieurong, chieudai));
 	}
 	
 }
---
+++ LAB/src/lab1/lab1_bai3.java
@@ -0,0 +1,9 @@
+package lab1;
+
+public class lab1_bai3 {
+
+    public static void main(String[] args){
+
+    }
+
+}
