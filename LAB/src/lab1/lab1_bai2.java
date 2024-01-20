82a7cb32d0f0c3f7bf980566788a0021aa395669
KhoaTrinh18 <126164079+KhoaTrinh18@users.noreply.github.com>
2024-01-20 19:48:38 +0700

    init

---
+++ LAB/src/lab1/lab1_bai2.java
@@ -0,0 +1,14 @@
+package lab1;
+
+import java.util.Scanner;
+
+public class lab1_bai2 {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Chiều dài: ");
+		float chieudai = scanner.nextFloat();
+		
+	}
+	
+}
